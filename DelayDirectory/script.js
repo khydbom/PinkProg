5a398c1e7cb7e5c14b9b8ae03b15f8944f383ca7
dbkwolf <33721623+dbkwolf@users.noreply.github.com>
2018-02-25 14:59:40 +0100

    finished workshop edits

--- DelayDirectory/script.js
+++ DelayDirectory/script.js
@@ -1,5 +1,48 @@
-console.log("hey there");
+console.log("hey there")
 
-function changeCOlor() {
-    console.og("I clicked");
+startTime();
+
+
+function startTime() {
+    var today= new Date()
+    var h = today.getHours()
+    var m = today.getMinutes()
+    var s = today.getSeconds()
+
+    m = checkTime(m)
+    s=checkTime(s)
+    $("#clock").html(h + ":" + m + ":" + s)
+    setTimeout(startTime, 500)
+
 }
+
+function checkTime(number) {
+    if (number<10){
+        number = "0" + number
+    }
+
+    return number
+}
+
+function changeColor() {
+    console.log("I clicked");
+    $("body").css("background-color", "pink")
+}
+
+function startBomb() {
+    var bombSeconds = 10
+    tick(bombSeconds)
+}
+
+function tick(bombSeconds) {
+    bombSeconds = bombSeconds -1
+        setTimeout( function () {
+            if (bombSeconds === 0) {
+                $("#bomb").html("Boom!")
+            } else {
+                tick(bombSeconds)
+                $("#bomb").html(bombSeconds)
+            }
+        }, 1000)
+       
+    }
